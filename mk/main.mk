12db9e6a78eb25e9a27077a6372b4bf154eb6ef4
Dylan McKay <dylanmckay34@gmail.com>
2016-09-26 14:32:56 +1300

    [AVR] Build the ObjCARC library in LLVM
    Rust depends on it, but does not link it currently.

--- mk/main.mk
+++ mk/main.mk
@@ -287,7 +287,7 @@ endif
 
 LLVM_OPTIONAL_COMPONENTS=x86 arm aarch64 avr mips powerpc pnacl systemz jsbackend msp430
 LLVM_REQUIRED_COMPONENTS=ipo bitreader bitwriter linker asmparser mcjit \
-                interpreter instrumentation
+                interpreter instrumentation objcarcopts
 
 ifneq ($(CFG_LLVM_ROOT),)
 # Ensure we only try to link targets that the installed LLVM actually has:
